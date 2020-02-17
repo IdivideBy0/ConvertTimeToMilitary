c9761daba7179a79c28d09c66d9de631ba8b66fb
Michael Murray <v823061@global.ual.com>
2020-02-17 11:00:34 -0600

    commit

--- ConvertTimeToMilitary/Program.cs
+++ ConvertTimeToMilitary/Program.cs
@@ -11,6 +11,18 @@ namespace ConvertTimeToMilitary
         static void Main(string[] args)
         {
 
+            Convert();
+                
+
+        }
+
+
+        private static void Convert()
+        {
+
+
+            while (true)
+            {
 
                 Console.WriteLine("Enter a standard (non-military) time." + Environment.NewLine);
 
@@ -31,8 +43,8 @@ namespace ConvertTimeToMilitary
                 UInt16 hour = UInt16.Parse(uTimeAry[0].ToString());
                 UInt16 min = UInt16.Parse(uTimeAry[1].ToString());
 
-            //If you want to convert standard time to military time, add 1200 to any time from 1:00pm to 11:00pm.
-            //So if you want to say 6:30pm in military lingo, add 1200 to 6:30 to get 1830.
+                //If you want to convert standard time to military time, add 1200 to any time from 1:00pm to 11:00pm.
+                //So if you want to say 6:30pm in military lingo, add 1200 to 6:30 to get 1830.
 
                 const UInt16 milAddValueHrs = 12;
                 const UInt16 milAddValueMins = 00;
@@ -43,7 +55,7 @@ namespace ConvertTimeToMilitary
                     min += milAddValueMins;
                 }
                 else
-                {                   
+                {
                     if (hour == 12 && daynight == "am")
                     {
                         hour = 0;
@@ -51,17 +63,19 @@ namespace ConvertTimeToMilitary
 
                 }
 
-            string hrStr = hour.ToString();
-            string minStr = min.ToString();
+                string hrStr = hour.ToString();
+                string minStr = min.ToString();
 
-            if (hour.ToString().Length == 1)
-                hrStr = "0" + hrStr;
+                if (hour.ToString().Length == 1)
+                    hrStr = "0" + hrStr;
 
-            if (min < 10)
-                minStr = "0" + min.ToString();
+                if (min < 10)
+                    minStr = "0" + min.ToString();
 
-            
-            Console.WriteLine(hrStr + minStr);
+
+                Console.WriteLine(hrStr + minStr);
+
+            }// End while
 
         }
 
